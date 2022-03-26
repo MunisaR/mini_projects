ada95068a36fa8fb7c69e7ae3752b9a315584066
MunisaR <munisrahmatullayeva04@gmail.com>
2022-03-26 17:07:09 +0500

    commit

--- Rotating Nav/script.js
+++ Rotating Nav/script.js
@@ -2,5 +2,12 @@ const open = document.getElementById('open')
 const close = document.getElementById('close')
 const container = document.querySelector('.container')
 
-open.addEventListener('click', () => container.classList.add('show-nav'))
-close.addEventListener('click', () => container.classList.remove('show-nav'))
+open.addEventListener('click', () => {
+    container.classList.add('show-nav')
+    container.classList.add('rotate')
+
+})
+close.addEventListener('click', () => {
+    container.classList.remove('show-nav')
+    container.classList.remove('rotate')
+})
