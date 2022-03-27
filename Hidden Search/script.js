bcb23daaf389fc0162adcdcde28d7da19eb372d8
MunisaR <munisrahmatullayeva04@gmail.com>
2022-03-27 21:15:46 +0500

    commit

--- Hidden Search/script.js
+++ Hidden Search/script.js
@@ -2,3 +2,7 @@ const input = document.querySelector('.input')
 const btn = document.querySelector('.btn')
 const search = document.querySelector('.search')
 
+btn.addEventListener('click', () => {
+    search.classList.add('active')
+    input.focus()
+})
