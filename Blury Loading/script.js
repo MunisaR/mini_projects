ba772fe2de79852d88f4ad22d70544be09189f05
MunisaR <munisrahmatullayeva04@gmail.com>
2022-04-08 00:03:15 +0500

    commit

--- Blury Loading/script.js
+++ Blury Loading/script.js
@@ -1,17 +1,19 @@
- const loadtext = document.querySelector('.load-text')
- const bg = document.querySelector('.bg')
+const loadtext = document.querySelector(".load-text");
+const bg = document.querySelector(".bg");
 
- let load = 0
+let load = 0;
 
- let int = setInterval(blurring, 30)
- function blurring() {
-     load++
+let int = setInterval(blurring, 30);
+function blurring() {
+  load++;
 
+  if (load > 99) {
+    clearInterval(int);
+  }
+  loadtext.innerText = "${load}%";
+  loadtext.style.opacity = scale(load, 0, 100, 1, 0);
+}
 
-     if (load > 99) {
-         clearInterval(int)
-     }
-     loadtext.innerText = '${load}%'
-     loadtext.style.opacity = scale(load, 0, 100, 1, 0) 
- }
- 
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
